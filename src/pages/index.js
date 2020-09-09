f774bf7e9beb180c6b3335e710979b1f077bc137
ColeyG <cjgeerts@gmail.com>
2020-09-09 12:33:22 -0400

    Lightbox and Frontend

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { graphql } from 'gatsby';
 
 export default ({ data }) => {
   const { allImage } = data;
   const { edges } = allImage;
 
+  const [jumboState, setJumboState] = useState(false);
+  const [jumboImage, setJumboImageState] = useState('');
+
   let images = [];
+  let jumbo = null;
+
+  const imageClickHandler = (e) => {
+    e.preventDefault();
+
+    console.log(e.target.src);
+    setJumboImageState(e.target.src);
+
+    // console.log('image clicked');
+    setJumboState(!jumboState);
+  };
+
+  const jumboClickHandler = (e) => {
+    e.preventDefault();
+    setJumboState(!jumboState);
+  };
 
   edges.forEach((image, index) => {
-    images.push(<img key={index} className="photo p-2 m-2 shadow-lg" src={require(`../../images/${image.node.path}`)} />);
+    images.push(<div className="w-full" key={index}><img className="photo object-cover" onClick={imageClickHandler} src={require(`../../images/${image.node.path}`)} /></div>);
   });
 
+  if (jumboState) {
+    jumbo = <div onClick={jumboClickHandler} className="jumbo-box bg-mint bg-opacity-25 sticky top-0 left-0 z-10 w-screen h-screen p-1">
+      <img src={jumboImage} />
+    </div>;
+  }
+
   return (
-    <div className="wrapper">
-      <div className="images flex flex-wrap justify-center sm:flex-col lg:flex-row items-center">
-        {images}
+    <div className="light-box relative w-screen h-screen overflow-x-hidden">
+      {jumbo}
+      <div className="wrapper">
+        <div className="images grid sm:grid-cols-1 md:grid-cols-3">
+          {images}
+        </div>
       </div>
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,25 @@
 module.exports = {
-  purge: [],
+  purge: {
+    layers: ['utilities'],
+    content: [
+      './pages/',
+    ],
+  },
   theme: {
+    colors: {
+      blue: '#473198',
+      mint: '#9CFC97',
+      green: '#6BA368',
+      darkgreen: '#515B3A',
+      shadowgreen: '#353D2F',
+      black: '#292929',
+    },
     extend: {},
   },
   variants: {},
   plugins: [],
+  future: {
+    removeDeprecatedGapUtilities: true,
+    purgeLayersByDefault: true,
+  },
 };
